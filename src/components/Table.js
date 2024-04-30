fb6bb185f82a268a136ece8ae5d14fcadc7b4654
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-04-30 07:26:09 +0300

    search functionality working

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,11 +4,32 @@ import { render } from "@testing-library/react";
 // display BankData based on what is searched for if empty display everything
 // change key to sth other than index
 function Table ({searchedData}){
-const [display,setDisplay]=useState(null)
+//const [display,setDisplay]=useState(BankData)
 
-searchedData.length === 0 ? { currentObj =[...Bankdata]
-  setDisplay(currentObj)}:console.log(searchedData)
-console.log(display)
+function displayData (){
+  if(searchedData.length === 0){
+    return BankData.map( (info,index) =>(
+    <tr key={index}>
+    <td>{info.Date}</td>
+    <td>{info.Description}</td>
+    <td>{info.Category}</td>
+    <td>{info.Amount}</td>
+    </tr>
+    ))
+  } else{
+    const filter = BankData.filter((info)=>info.Description.trim().toLowerCase().includes(searchedData.trim().toLowerCase()))
+    console.log(filter)
+    return filter.map( (info,index) =>(
+      <tr key={index}>
+      <td>{info.Date}</td>
+      <td>{info.Description}</td>
+      <td>{info.Category}</td>
+      <td>{info.Amount}</td>
+      </tr>
+      ))
+
+  }
+}
  return (
   
     <table id="dataDisplay">
@@ -20,17 +41,10 @@ console.log(display)
             <th>Amount</th>
         </tr>
         </thead>
-        <tbody>
-          {BankData.map((info,index) =>
-    <tr key={index}>
-    <td>{info.Date}</td>
-    <td>{info.Description}</td>
-    <td>{info.Category}</td>
-    <td>{info.Amount}</td>
-    </tr>)
-    }
+    <tbody>
+        {displayData()}
     </tbody>
     </table>
- )
+    )
 }
 export default Table
